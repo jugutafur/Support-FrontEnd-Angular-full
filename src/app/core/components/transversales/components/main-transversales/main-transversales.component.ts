53ed9d98cc6e3ba93ab0ebaa8d49002c64b14ec5
DESKTOP-8GLU08G\HP <juantafur.25@gmail.com>
2021-11-15 21:23:07 -0500

    add features to csharp

---
+++ src/app/core/components/transversales/components/main-transversales/main-transversales.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-transversales',
+  templateUrl: './main-transversales.component.html',
+  styleUrls: ['./main-transversales.component.scss']
+})
+export class MainTransversalesComponent  {
+
+  lista= ["JAVA", "VB" , "C#", "F#"]
+
+}
