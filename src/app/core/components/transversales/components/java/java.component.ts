ead4d2890405b10794738c0c087376a72c7ed006
DESKTOP-8GLU08G\HP <juantafur.25@gmail.com>
2021-12-12 22:54:17 -0500

    add new featres Java

--- src/app/core/components/transversales/components/java/java.component.ts
+++ src/app/core/components/transversales/components/java/java.component.ts
@@ -7,4 +7,23 @@ import { Component } from '@angular/core';
 })
 export class JavaComponent {
 
+  public abrellave: string;
+  public cierrallave: string;
+  public abreDiamante: string;
+  public cierraDiamante: string;
+  public abreParentesis: string;
+  public cierraParentesis: string;
+  public abreCuadro: string;
+  public cierraCuadro: string;
+
+  constructor() {
+    this.abrellave= "{";
+    this.cierrallave="}";
+    this.abreDiamante= "<";
+    this.cierraDiamante= ">";
+    this.abreParentesis= "(";
+    this.cierraParentesis= ")";
+    this.abreCuadro= "[";
+    this.cierraCuadro= "]";
+  }
 }
