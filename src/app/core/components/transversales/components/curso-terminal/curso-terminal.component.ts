1cff52a6b7c94a1c300e4d543c8c0616342535c8
jugutafur <juantafur.25@gmail.com>
2023-07-05 20:51:34 -0500

    add windows server

--- src/app/core/components/transversales/components/curso-terminal/curso-terminal.component.ts
+++ src/app/core/components/transversales/components/curso-terminal/curso-terminal.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-curso-terminal',
   templateUrl: './curso-terminal.component.html',
   styleUrls: ['./curso-terminal.component.scss']
 })
-export class CursoTerminalComponent implements OnInit {
+export class CursoTerminalComponent{
 
-  constructor() { }
+  public abrellave: string;
+  public cierrallave: string;
+  public abreDiamante: string;
+  public cierraDiamante: string;
+  public abreParentesis: string;
+  public cierraParentesis: string;
+  public abreCuadro: string;
+  public cierraCuadro: string;
 
-  ngOnInit(): void {
+  constructor() {
+    this.abrellave= "{";
+    this.cierrallave="}";
+    this.abreDiamante= "<";
+    this.cierraDiamante= ">";
+    this.abreParentesis= "(";
+    this.cierraParentesis= ")";
+    this.abreCuadro= "[";
+    this.cierraCuadro= "]";
   }
 
 }
